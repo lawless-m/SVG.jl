230a1dbcfa3ba84497d65e942a00e02dd2d7d8e3
matt <matt@huge.steponnopets.net>
2018-03-23 21:37:51 +0000

    works

--- SVG.jl
+++ SVG.jl
@@ -1,22 +1,23 @@
-module svg
+module SVG
 
-function opensvg(io, h::Float64, w::Float64)
+
+function open(io, h::Float64, w::Float64)
 	@printf(io, "<svg height=\"%0.2f\" width=\"%0.2f\">\n", w, h)
 end
 
 function polyline(fid, pts::Vector{Tuple{Real, Real}}, style::String)
-	@printf(fid, "<polyline points=\"")
+	println(fid, "<polyline points=\"")
 	for (x,y) in pts
-		@printf(fid, "%0.2f, %0,2f ", x, y)
+		@printf(fid, "%0.2f, %0.2f ", x, y)
 	end
-	@printf("\" style=\"%s\" />\n")
+	@printf(fid, "\" style=\"%s\" />\n", style)
 end
 
 function blackline(w::Real)
 	@sprintf("fill:none;stroke:black;stroke-width:%0.2f", w)
 end
 
-function closesvg(io, h::Float64, w::Float64)
+function close(io)
 	println(io, "</svg>")
 end
 
