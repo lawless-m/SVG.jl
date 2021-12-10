a40733ced9fc88295204f861387efffa8b67d3f3
lawless-m <matt-github@steponnopets.net>
2021-12-10 16:45:22 +0000

    added scaling functions and tests for them

--- src/SVG.jl
+++ src/SVG.jl
@@ -1,6 +1,6 @@
 module SVG
 
-export Style, Svg, Polyline, Circle, Line, write, scale, translate, bounds
+export Style, Svg, Polyline, Circle, Line, write, scale, scale_fn, translate, bounds
 
 dp(n; digits=2) = round(n; digits)
 
@@ -13,6 +13,11 @@ struct Style
 	Style(;fill="none", strokecolor="black", strokewidth=1) = new(fill, strokecolor, strokewidth)
 end
 
+function scale_fn(ptop, pmax, pmin)
+	scale = ptop / (pmax - pmin)
+	n -> scale * (n - pmin)
+end
+
 struct Polyline <: SvgObject
 	xs
 	ys 
@@ -21,6 +26,7 @@ struct Polyline <: SvgObject
 	Polyline(npoints::Int; style::Style=Style()) = new(Vector{Float64}(undef, npoints), Vector{Float64}(undef, npoints), style)
 	Polyline(xs, ys; style::Style=Style()) = new(xs, ys, style)
 end
+scale(p::Polyline, scalefn) = Polyline(map(scalefn, p.xs), map(scalefn, p.ys); p.style)
 
 struct Line <: SvgObject
 	x1
@@ -30,6 +36,7 @@ struct Line <: SvgObject
 	style::Style
 	Line(x1, y1, x2, y2; style::Style=Style()) = new(x1, y1, x2, y2, style)
 end
+scale(l::Line, scalefn) = Line(scalefn(l.x1), scalefn(l.y1), scalefn(l.x2), scalefn(l.y2), style=l.style)
 
 struct Circle <: SvgObject
 	x
@@ -38,6 +45,8 @@ struct Circle <: SvgObject
 	s::Style
 	Circle(x, y, r; style::Style=Style()) = new(x, y, r, style)
 end
+
+scale(c::Circle, scalefn) = Circle(scalefn(c.x), scalefn(c.y), scalefn(0) - scalefn(c.r), style=c.s)
 
 struct Svg
 	width
@@ -92,7 +101,6 @@ points2circles(xs, ys; r=1) = map(i->Circle(xs[i], ys[i], r), 1:length(xs))
 
 bounds(c::Circle) = (xmin=c.x-c.r, xmax=c.x+c.r, ymin=c.y-c.r, ymax=c.y+c.r)
 
-
 function bounds(svg::Svg)
 	xmin, xmax, ymin, ymax = Inf, -Inf, Inf, -Inf
 	for b in map(bounds, svg.objects)
@@ -112,20 +120,11 @@ function bounds(svg::Svg)
 	(;xmin, xmax, ymin, ymax)
 end
 
-## helper
-function scale(points, ptop; pmin=nothing, pmax=nothing)
-	if pmin === nothing
-		pmin = minimum(points)
-	end
-	if pmax === nothing
-		pmax = maximum(points)
-	end
-	scale = ptop / (pmax - pmin)
-	map(n -> scale * (n-pmin), points)
-end
+scale(points::Vector, ptop::Float64) = map(scale_fn(ptop, maximum(points), minimum(points)), points)
+scale(points::Vector, fn::Function) = map(fn, points)
 
 #helper
-translate(points, delta) = map(p->p+delta, points)
+translate(points::Vector, delta) = map(p->p+delta, points)
 
 
 ###
--- test/runtests.jl
+++ test/runtests.jl
@@ -8,6 +8,15 @@ function wtest(s)
     read(z, String) == "<svg width=\"0.0\" height=\"10.0\">\n<circle cx=\"0.0\" cy=\"9.0\" r=\"1.0\" style=\"fill:none;stroke:black;stroke-width:1.0\" />\n<line x1=\"0.0\" y1=\"9.0\" x2=\"1.0\" y2=\"8.0\" style=\"fill:none;stroke:black;stroke-width:1.0\" />\n<polyline points=\"0.0, 7.0 1.0, 6.0 2.0, 5.0 \" style=\"fill:none;stroke:black;stroke-width:1.0\" />\n</svg>\n"
 end
 
+function polytest(sfn)
+    p1 = scale(Polyline([0,5,10], [10,5,0]), sfn)
+    p2 = Polyline([100.0, 50.0, 0.0], [0.0, 50.0, 100.0])
+    p1.xs == p2.xs && p1.ys == p1.ys && p1.style == p2.style
+end
+
+ps = [0, 5, 10]
+sfn = scale_fn(100, minimum(ps), maximum(ps))
+
 c = Circle(0,1,1)
 l = Line(0,1,1,2)
 p = Polyline([0,1,2], [3,4,5])
@@ -19,4 +28,9 @@ s = Svg(0, 10, [c,l,p])
    @test bounds(p) == (xmin=0, xmax=2, ymin=3, ymax=5)
    @test bounds(s) == (xmin=-1, xmax=2, ymin=0, ymax=5)
    @test wtest(s)
+   @test sfn(0) == 100 && sfn(10) == 0
+   @test scale(Circle(5,5,1), sfn) == Circle(50.0, 50.0, 10.0)
+   @test scale(Line(0,0,10,10), sfn) == Line(100.0, 100.0, -0.0, -0.0)
+   @test scale([0,5,10], sfn) == [100, 50, 0]
+   @test polytest(sfn)
 end
